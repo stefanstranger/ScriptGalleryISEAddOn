f684a399b06e44c8f75bbfafcf91877deee5fef8
Stefan Stranger <stefan.stranger@outlook.com>
2014-07-14 15:49:39 +0200

    Initial version of PowerShell ScriptGalleryISEAddOn

---
+++ ScriptGalleryISEAddOn/ScriptGalleryISEAddOn/UserControl1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.PowerShell.Host.ISE;
+
+namespace ScriptGalleryISEAddOn
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ScriptGallery : UserControl, IAddOnToolHostObject
+    {
+        public ScriptGallery()
+        {
+            InitializeComponent();
+        }
+
+        // Populated by the ISE because we implement the IAddOnToolHostObject interface.
+        // Represents the entry-point to the ISE object model.
+        public ObjectModelRoot HostObject
+        {
+            get;
+            set;
+        }
+    }
+}
