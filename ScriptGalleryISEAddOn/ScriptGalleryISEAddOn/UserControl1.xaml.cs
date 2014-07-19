d7c7c8733924bd5f83a006c9cd307ef66b6fc3d6
Stefan Stranger <stefan.stranger@outlook.com>
2014-07-19 11:22:39 +0200

    Improvements
    Made some improvements

--- ScriptGalleryISEAddOn/ScriptGalleryISEAddOn/UserControl1.xaml.cs
+++ ScriptGalleryISEAddOn/ScriptGalleryISEAddOn/UserControl1.xaml.cs
@@ -2,6 +2,11 @@
 using System.Windows.Navigation;
 using Microsoft.PowerShell.Host.ISE;
 using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
+// using System.Management.Automation; use for AST
+// using System.Management.Automation.Language;
 
 namespace ScriptGalleryISEAddOn
 {
@@ -37,7 +42,45 @@ namespace ScriptGalleryISEAddOn
         {
             var fileName = HostObject.CurrentPowerShellTab.Files.SelectedFile.FullPath;
             Clipboard.SetData(DataFormats.Text, (Object)fileName);
-            MessageBox.Show("File Name copied to clipboard");
+            // MessageBox.Show("File Name copied to clipboard");
+        }
+
+        private void GetScriptContent(object sender, RoutedEventArgs e)
+        {
+            var scriptContent = HostObject.CurrentPowerShellTab.Files.SelectedFile.Editor.Text;
+            StringBuilder sb = new System.Text.StringBuilder();
+            //MessageBox.Show(scriptContent,"Content");
+            try
+            {
+                Regex regexObj = new Regex(@"^#.*?$", RegexOptions.Singleline | RegexOptions.Multiline);
+                Match matchResults = regexObj.Match(scriptContent);
+                while (matchResults.Success)
+                {
+                    // matched text: matchResults.Value
+                    // match start: matchResults.Index
+                    // match length: matchResults.Length
+                    sb.AppendLine(matchResults.Value);
+                    matchResults = matchResults.NextMatch();
+                    
+                }
+
+                // MessageBox.Show(sb.ToString(),"Script Comments");
+                string pattern = "(\r\n)";
+                string replacement = "";
+                Regex rgx = new Regex(pattern);
+                string result = rgx.Replace(sb.ToString(), replacement);
+                MessageBox.Show(result,"Script Comments fixed");
+
+;
+
+                // MessageBox.Show("Script Comments copied to clipboard");
+                Clipboard.SetData(DataFormats.Text, (Object)result);
+                
+            }
+            catch (ArgumentException ex)
+            {
+                // Syntax error in the regular expression
+            }
         }
     }
 }
--- ScriptGalleryISEAddOn/ScriptGalleryISEAddOn/bin/Debug/LoadScriptGalleryISEAddOn.ps1
+++ ScriptGalleryISEAddOn/ScriptGalleryISEAddOn/bin/Debug/LoadScriptGalleryISEAddOn.ps1
@@ -1,4 +1,13 @@
-﻿add-type -path "C:\Users\stefstr\Documents\GitHub\PowerShell\ScriptGalleryISEAddOn\ScriptGalleryISEAddOn\ScriptGalleryISEAddOn\bin\Debug\ScriptGalleryISEAddOn.dll"
+﻿# ---------------------------------------------------
+# Script: C:\Users\stefstr\Documents\GitHub\PowerShell\ScriptGalleryISEAddOn\ScriptGalleryISEAddOn\ScriptGalleryISEAddOn\bin\Debug\LoadScriptGalleryISEAddOn.ps1
+# Version: 0.1
+# Author: Stefan Stranger
+# Date: 07/18/2014 17:04:10
+# Description:
+# Comments:
+# ---------------------------------------------------
+
+add-type -path "C:\Users\stefstr\Documents\GitHub\PowerShell\ScriptGalleryISEAddOn\ScriptGalleryISEAddOn\ScriptGalleryISEAddOn\bin\Debug\ScriptGalleryISEAddOn.dll"
 
 $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('ScriptGalleryUpload', [ScriptGalleryISEAddOn.ScriptGallery], $true)
 
